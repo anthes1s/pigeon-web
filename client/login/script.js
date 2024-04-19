6280da6db0c6d79a2e108ed5c82a97beac703e25
anthes1s <4nthesis@gmail.com>
2024-04-19 21:46:36 +0300

    finished auth front

--- client/login/script.js
+++ client/login/script.js
@@ -1,26 +1,48 @@
 'use strict';
 
-const buttonSignIn = document.getElementById('buttonSignIn');
+function showStatusLabel(message, color) {
+    const div = document.getElementById('form');
 
-buttonSignIn.addEventListener('click', async () => {
-    try{
-    const inputLogin = document.getElementById('inputLogin');
-    const inputPassword = document.getElementById('inputPassword');
+    let check = document.getElementById('statusLabel')
+    if(check) {
+        div.removeChild(check);
+    }
 
-    let response = await axios.post('/auth/login', {
-        username: inputLogin.value,
-        password: inputPassword.value,
+    const label = document.createElement('label');
+    label.id = "statusLabel";
+    label.textContent = message;
+    label.style.color = color;
+    
+    div.appendChild(label);
+}
+
+function init() {
+    const buttonSignIn = document.getElementById('buttonSignIn');
+    
+    buttonSignIn.addEventListener('click', async () => {
+        try { 
+            const inputLogin = document.getElementById('inputLogin');
+            const inputPassword = document.getElementById('inputPassword');
+
+            let response = await axios.post('/auth/login', {
+                username: inputLogin.value,
+                password: inputPassword.value,
+            });
+
+            console.log(response.data);
+
+            let success = response.data.success;
+            if(success) {
+            // Add JWT to the localStorage and redirect to the '/chat' route;
+                localStorage.setItem('jwt', response.data.jwt);
+                window.location.href = '/chat';
+            }
+            
+        } catch(error) {
+            if(error.response?.data?.message) showStatusLabel(error.response.data.message, 'Red');
+            else showStatusLabel(error.message, 'Red');
+        }
     });
+}
 
-    console.log(response.data);
-
-    let success = response.data.success;
-    if(success) {
-        // Add JWT to the localStorage and redirect to the '/chat' route;
-        localStorage.setItem('jwt', response.data.jwt);
-        window.location.href = '/chat';
-    }
-    }   catch(err) {
-        console.log(err.response.data.message);
-    }
-});
+init();
--- client/register/script.js
+++ client/register/script.js
@@ -1,21 +1,46 @@
 'use strict';
 
-const buttonSignUp = document.getElementById('buttonSignUp');
+function showStatusLabel(message, color) {
+    const div = document.getElementById('form');
 
-buttonSignUp.addEventListener('click', async () => {
-    try {
-    const inputLogin = document.getElementById('inputLogin');
-    const inputPassword = document.getElementById('inputPassword');
-    // Add some check that would require the length of password to be >8 symbols
-
-    let response = await axios.post('/auth/register', {
-        username: inputLogin.value,
-        password: inputPassword.value,
-    });
-
-    console.log(response.data);
-    // Let user know that the registration was successful
-    } catch(error) {
-        
+    let check = document.getElementById('statusLabel')
+    if(check) {
+        div.removeChild(check);
     }
-});
+
+    const label = document.createElement('label');
+    label.id = "statusLabel";
+    label.textContent = message;
+    label.style.color = color;
+    
+    div.appendChild(label);
+}
+
+function init() {
+    const buttonSignUp = document.getElementById('buttonSignUp');
+
+    buttonSignUp.addEventListener('click', async () => {
+        try {
+            const inputLogin = document.getElementById('inputLogin');
+            const inputPassword = document.getElementById('inputPassword');
+
+            if(inputPassword.value.length < 8) {
+                throw new Error("Your password is too short!");
+            }
+    
+            let response = await axios.post('/auth/register', {
+                username: inputLogin.value,
+                password: inputPassword.value,
+            });
+    
+            console.log(response.data);
+            showStatusLabel("Registration successful!", 'Green');
+            setTimeout(() => window.location.href = '/chat', 1000);
+        } catch(error) {
+            if(error.response?.data?.message) showStatusLabel(error.response.data.message, 'Red');
+            else showStatusLabel(error.message, 'Red');
+        }
+    });
+}
+
+init();
